7b629ff95527bb641d6ba60d8c318c7fa1830483
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-03-05 22:21:11 +0800

    Learn the use npm

--- my-custom-package/index.js
+++ my-custom-package/index.js
@@ -1,5 +1,8 @@
+const upperCase = require('upper-case').upperCase;
+
 function greet(name) {
-    console.log(`Hello ${name}, welcome to Codevolution`);
+    console.log(upperCase(`Hello ${name}, welcome to Codevolution`));
 }
 
+greet('vishwas');
 module.exports = greet;
