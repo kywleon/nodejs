43c26b9d3b1ef8eb839205a6b28a80b150c99fb1
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-03-06 18:27:53 +0800

    Learn node.js
    Learn cli option

--- my-custom-cli/index.js
+++ my-custom-cli/index.js
@@ -3,3 +3,18 @@
 
 // console.log('kywleon pokedex');
 
+// console.log(process.argv);
+
+const yargs = require('yargs');
+const { argv } = yargs(process.argv);
+
+const printFiveMoves = async (pokemonName) => {
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const pokemon = await response.json();
+    const moves = pokemon.moves.map( ({ move }) => move.name );
+    console.log(moves.slice(0,5));
+};
+
+printFiveMoves(argv.pokemon);
