71d44ab4c404d6068f00ea4945f9bf1e121942b5
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-03-07 01:34:51 +0800

    Update the index.js

--- index.js
+++ index.js
@@ -550,7 +550,7 @@ const server = http.createServer( (req, res) => {
     res.end('Hello World!');
 });
 
-const Port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen( PORT , () => console.log("Server is running on port 3000"));
 
